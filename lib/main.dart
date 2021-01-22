332d4a15f0717810a7da61a07c8bd0b13b377a6f
Sainath Chilkuri <chilkurisainath@gmail.com>
2021-01-22 16:34:10 +0530

    new features

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'generated/l10n.dart';
 void main() async {
   //Development Branch
   //features
+  //new feature
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(App());
